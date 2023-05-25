bcb0ad5747e0dc37e95c7bc16b3825d718c63038
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-05-25 12:38:53 -0500

    Added if condition to only increment our count in results if the item is found in the itemsToCount object

--- countOnly.js
+++ countOnly.js
@@ -16,15 +16,16 @@ const countOnly = function(allItems, itemsToCount) {
   let results = {};
 
 for (const item of allItems) {
+if (itemsToCount[item]) {
 if(results[item]) {
 results[item] += 1;
   } else {
       results[item] = 1;
     }
     }
+  }
   return results;
 };
-
 
 
 
