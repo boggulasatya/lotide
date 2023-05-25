b88f4fddc3e8dfee9a25d3c69a1c2272b32660d6
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-05-25 12:36:29 -0500

    Incrementing the count of each item into results

---
+++ countOnly.js
@@ -0,0 +1,48 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705\u2705\u2705 Assertion Passed : ${actual} === ${expected}`);
+  } else {
+    console.log(`\u{1f6d1}\u{1f6d1}\u{1f6d1} Assertion Failed : ${actual} !== ${expected}`);
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(5, 7);
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  let results = {};
+
+for (const item of allItems) {
+if(results[item]) {
+results[item] += 1;
+  } else {
+      results[item] = 1;
+    }
+    }
+  return results;
+};
+
+
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
