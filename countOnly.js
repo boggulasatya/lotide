8d771d9dfe7bf80d41d4922ff2d09954d553aed5
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-05-25 12:43:49 -0500

    Eslint code

--- countOnly.js
+++ countOnly.js
@@ -15,13 +15,13 @@ assertEqual(5, 7);
 const countOnly = function(allItems, itemsToCount) {
   let results = {};
 
-for (const item of allItems) {
-if (itemsToCount[item]) {
-if(results[item]) {
-results[item] += 1;
-  } else {
-      results[item] = 1;
-    }
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     }
   }
   return results;
