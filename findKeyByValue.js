89434aaaf2509c4ceb9b779b3975fd55ea5ac5b2
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-05-28 02:19:32 -0500

    findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705\u2705\u2705 Assertion Passed : ${actual} === ${expected}`);
+  } else {
+    console.log(`\u{1f6d1}\u{1f6d1}\u{1f6d1} Assertion Failed : ${actual} !== ${expected}`);
+  }
+};
+
+
+//Implement the function findKeyByValue which takes in an object and a value. It should scan the object and return the first key which contains the given value. If no key with that given value is found, then it should return undefined.
+
+
+const findKeyByValue = function(obj, value) {
+  for (const keys of Object.keys(obj)) {
+    if (obj[keys] === value) {
+      return keys;
+
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+const cityByProvince = {
+  ontario: "Ottawa",
+  manitoba: "Winnipeg",
+  alberta: "Calgary",
+  BC: "Victoria"
+};
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "drama");
+assertEqual(findKeyByValue(cityByProvince, "Calgary"), "alberta");
+assertEqual(findKeyByValue(cityByProvince, "Victoria"), "manitoba");
