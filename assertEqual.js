77afeae41a1e1c1b570c3baf350a91f276f1aa66
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-06-07 17:07:03 -0500

    Refactored assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -5,7 +5,5 @@ const assertEqual = function(actual, expected) {
     console.log(`\u{1f6d1}\u{1f6d1}\u{1f6d1} Assertion Failed : ${actual} !== ${expected}`);
   }
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(5, 7);
+
+module.exports = assertEqual;
