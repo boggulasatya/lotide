4e6094b06d07d9348c39cfcd732777b2160be828
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-06-07 17:15:31 -0500

    refactored tail

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 const tail = function(arr) {
   if (arr.length <= 1) {
     return [];
@@ -12,17 +6,7 @@ const tail = function(arr) {
     return arr.slice(1);
   }
 };
-
-const output = tail(["Yo Yo"]);
-assertEqual(output.length, 0);
-
-const final = tail([]);
-assertEqual(final.length, 0);
-
-const result = tail(["Yo Yo", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail;
 
 
 
