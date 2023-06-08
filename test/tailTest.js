bc09ec7a71af01c38432629f4e1a5697bba35c41
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-06-07 19:24:47 -0500

    Refactored tailTest.js & middleTest.js with Mocha & chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,22 @@
 
-
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const output = tail(["Yo Yo"]);
+
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  })
+});
+
+
+/*const output = tail(["Yo Yo"]);
 assertEqual(output.length, 0);
 
 const final = tail([]);
@@ -12,4 +25,4 @@ assertEqual(final.length, 0);
 const result = tail(["Yo Yo", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+assertEqual(result[1], "Labs");*/ // ensure second element is "Labs"//
