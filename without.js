c7da6054a5435fd6c23cf5c027e4fff7e5d4efd8
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-06-13 17:05:58 -0500

    removed Test cases

--- without.js
+++ without.js
@@ -1,34 +1,3 @@
-
-const eqArrays = function(a1, a2) {
-   if(!Array.isArray(a1) || !Array.isArray(a2)) {
-    return false;
-   } 
-   else if (a1.length !== a2.length) {
-      return false;
-    }
-
-    for (let i = 0; i < a1.length; i++) {
-      if (a1[i] !== a2[i]) {
-        return false;
-      }
-    }
-    return true;
-      };
-  
-
-
-
-  const assertArraysEqual = function(b1, b2) {
-  
-    if (eqArrays(b1, b2)) {
-      console.log("Assertion Passed: Arrays are equal");
-      return true;
-    } else {
-      console.log("Assertion Failed: Arrays are not equal");
-      return false;
-    }
-  };
-
 /* take 2 arrays Source & items to remove as parameters for without function & print new array exclude elements from items to remove */
 
 const without = function(sourceA, itemsToRemove){
@@ -41,9 +10,3 @@ const without = function(sourceA, itemsToRemove){
     return output;
 };
 module.exports = without;
-const words = ["hello", "world", "lighthouse"];
-console.log(without(words, ["lighthouse"])); 
-console.log(without([1, 2, 3], [1])) // => [2, 3]
-console.log(without([1, 2, 3], [1, 2]));
-console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
-console.log(assertArraysEqual(words, ["hello", "world", "lighthouse"]));
